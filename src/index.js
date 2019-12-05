8fb9a61b3b0fc6cb8b466994f4341179049f77c5
prabin <prabintp@gmail.com>
2019-12-05 12:48:19 +0530

    Initial commit with template

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './style.scss';
+//import './desktop-style.css';
+
+/*import _ from 'lodash';
+
+
+  function component() {
+    const element = document.createElement('div');
+
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('heading');
+
+    return element;
+  }
+
+  document.body.appendChild(component()); */
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.js'
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+     new CleanWebpackPlugin(),
+     new HtmlWebpackPlugin({
+      title: 'pranav sruthi',
+      template: "./src/index.html",
+      filename: "./index.html"
+     }),
+   ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+       rules: [
+      {
+        test: /\.s[ac]ss$/i,
+             use: [
+               'style-loader',
+               'css-loader',
+               'sass-loader',
+             ],
+     },
+     {
+             test: /\.(png|svg|jpg|gif)$/,
+             use: [
+           'file-loader',
+             ],
+           },
+         ],
+       },
+};
